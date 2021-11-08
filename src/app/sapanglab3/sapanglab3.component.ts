a910fc5bed5741126d883b72b44f70af92883071
IronKriston <kristonsapang@gmail.com>
2021-11-08 10:39:11 +0800

    1 file modified, 2 added

---
+++ src/app/sapanglab3/sapanglab3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sapanglab3',
+  templateUrl: './sapanglab3.component.html',
+  styleUrls: ['./sapanglab3.component.css']
+})
+export class Sapanglab3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
